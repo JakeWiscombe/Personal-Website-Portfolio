85f3d0129a9ecfb34a1ed34da79984991ed15dee
JakeWiscombe <JAKE.WISCOMBE24@bathspa.ac.uk>
2025-03-27 16:00:31 +0000

    Started working on Header
    Had an issue where the background image wasn't working. Also need to finish working on the button groups.

--- jxmodifications/javascript/navigation.js
+++ jxmodifications/javascript/navigation.js
@@ -2,6 +2,7 @@ function navDropdown() {
     document.getElementById("jxDropdown").classList.toggle("show");
   }
   
+  
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(e) {
     if (!e.target.matches('.dropdown-button')) {
